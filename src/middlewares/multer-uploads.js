2fba168e51af3b0aad78f53997dc30db0cc6cab7
MoralesR-2020045 <rmorales215327@gmail.com>
2025-06-26 10:37:12 -0600

    feat: funcionalidades de archivos en task

--- src/middlewares/multer-uploads.js
+++ src/middlewares/multer-uploads.js
@@ -3,6 +3,7 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "cloudinary";
 import { extname } from "path";
 
+
 cloudinary.v2.config({
   cloud_name: "ddchtdi5y",
   api_key: "598451487432635",
@@ -21,6 +22,37 @@ const profileImageStorage = new CloudinaryStorage({
   },
 });
 
+const storageTaskFiles = new CloudinaryStorage({
+  cloudinary: cloudinary.v2,
+  params: {
+    folder: "taskFilesKivora",
+    public_id: (req, file) => {
+      const fileExtension = extname(file.originalname);
+      const fileName = file.originalname.split(fileExtension)[0];
+      return `${fileName}-${Date.now()}`;
+    },
+    allowed_formats: ["jpg", "png", "jpeg", "webp", "pdf", "docx", "xlsx", "pptx"],
+  },
+});
+
+const taskFileFilter = (req, file, cb) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",    
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation" 
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Tipo de archivo no permitido"));
+  }
+};
+
 export const uploadProfilePicture = multer({
   storage: profileImageStorage,
   fileFilter: (req, file, cb) => {
@@ -29,4 +61,10 @@ export const uploadProfilePicture = multer({
   limits: {
     fileSize: 10000000,
   },
+});
+
+export const uploadTaskFiles = multer({
+  storage: storageTaskFiles,
+  fileFilter: taskFileFilter,
+  limits: { fileSize: 10 * 1024 * 1024 }, 
 });
